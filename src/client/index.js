cbc6f46a618bf73187bba2f4ba19df5b5ee45dfd
jibanezredp <jeremy.ibanez@redpelicans.com>
2016-07-06 17:41:00 +0200

    two day of work, atomic commits coming next time

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import TodoApp from './components/TodoApp'
 
-const App = () => (<h1> A fantastic Todo list will be here soon ... </h1>)
+const LISTS = [
+  { id: 0, title: 'To do', tasks: [
+      { id: 0, text: 'buy eggs' },
+      { id: 1, text: 'buy milk' },
+      { id: 2, text: 'call bank' },
+      { id: 6, text: 'watch got' },
+  ] },
+  { id: 1, title: 'Sprint one', tasks: [
+      { id: 3, text: 'stateless components' },
+      { id: 4, text: 'write tests' },
+      { id: 5, text: 'flexbox' },
+  ] },
+];
 
-ReactDom.render(<App/>, document.getElementById('todo'))
+ReactDom.render(
+  <TodoApp lists={LISTS} />,
+  document.getElementById('todo')
+)
