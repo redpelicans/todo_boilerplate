43a495447008e3256821efdbbf59f0ed58e83c7f
edouard lacombe <edouard.lacombe@redpelicans.com>
2016-07-07 11:37:20 +0200

    index

--- src/client/index.js
+++ src/client/index.js
@@ -3,16 +3,14 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 const fake = [
-    { id: 1, title: 'List1', tasks: [ { id: 1, task: '1st' },
-    									 { id: 2, task: '2nd'},
-    									 { id: 3, task: '3rd'}
-    								]
+    { id: 1, title: 'List1', tasks: [{ id: 1, task: '1st' },
+                                    { id: 2, task: '2nd' },
+                                    { id: 3, task: '3rd' }],
     },
-    { id: 2, title: 'List2', tasks: [ { id: 4, task: '4th' },
-    									 { id: 5, task: '5th'},
-    									 { id: 6, task: '6th'}
-    								]
-    }
+    { id: 2, title: 'List2', tasks: [{ id: 4, task: '4th' },
+                                    { id: 5, task: '5th' },
+                                    { id: 6, task: '6th' }],
+    },
 ];
 
 ReactDom.render(
