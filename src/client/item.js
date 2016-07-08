28caa2f2540da7503fee39cab283b8357464fa7a
Marianne Porcel <marianneporcel@hotmail.com>
2016-07-08 18:11:34 +0200

    commit item.js

--- src/client/item.js
+++ src/client/item.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import TodoApp from './app'
 import RemoveItem from './removeitem'
 
-const Item = ({ name }) => <li className='item'>{ name }<RemoveItem/></li>
+const Item = ({ name, removeItemClick }) => <li className='item'>{ name }<RemoveItem removeItemClick={removeItemClick} /></li>
 
 Item.propTypes = {
   name: React.PropTypes.string.isRequired,
+  removeItemClick: React.PropTypes.func.isRequired,
+  
+  
 };
 
 export default Item
