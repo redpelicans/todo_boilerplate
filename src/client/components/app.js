3fcaa59ed9dce2bd5b1608f4a7f6a8967a47c68e
edouard lacombe <edouard.lacombe@redpelicans.com>
2016-07-08 08:46:32 +0200

    oops

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -1,11 +1,57 @@
 import React from 'react'
 import TaskLists from './tasklists'
 import Title from './title'
+import CustomButton from './custombutton'
+import Task from './task'
 
-const App = (props) =>
-    <div className='app'>
-        <Title title='App' />
-        <TaskLists {...props} />
-    </div>
+class App extends React.Component {
+  constructor (props) {
+  	super();
+  	this.state = {
+  		onClick: false,
+  		onEnter: false
+  	};
+
+  }
+  
+  leOnClick () {
+  	console.log('vas y click ! click j\'te dis !');
+ 	this.setState({ onClick: true });
+  }
+
+  leOnKeyPress (e) {
+  	console.log('il appuie sur des touches et il est content....');
+  	if (e.key === 'Enter') {
+  		console.log('putain c\'était pas compliqué bordel !');
+ 		this.setState({ onKEnter: true });
+ 	}
+  }
+
+  onButtonSelected (button) {
+  	console.log('Ah t\'as trouvé le bouton enfin.... : ');
+  	console.log(button);
+  	if (button == 'addtask') {
+  		return <Task />
+  	};
+  }
+
+  render () {
+  	var classes = ([
+  	  this.state.onClick && 'click',
+  	  this.state.onEnter && 'enter'
+  	]);
+
+  	return (
+    	<div
+    	className='datApp'
+    	onClick={this.leOnClick.bind(this)}
+    	onKeyPress={this.leOnKeyPress.bind(this)}>
+    		<CustomButton name='addlist' message='Ajouter une liste' onButtonSelected={this.onButtonSelected} />
+        	<Title title='App' />
+        	<TaskLists {...this.props} onButtonSelected={this.onButtonSelected} />
+    	</div>
+  	);
+  }
+}
 
 export default App
