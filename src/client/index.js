4e0e28dd1f392f27ab2bea0a0d20f2a9b9102a1f
Antoine BARBAROUX <antoine.barbaroux@redpelicans.com>
2016-07-09 19:45:39 +0200

    First working code (single todo)
    - Added single task data placeholder
    - Added Task renderer
    - Integrated task in app for testing

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import _ from 'lodash'
 
-const App = () => (<h1>A fantastic Todo list will be here soon ... </h1>)
+const testTask = {
+    id: 0,
+    title: 'test',
+    description: 'Lorem ipsum',
+    checked: false,
+}
 
-ReactDom.render(<App/>, document.getElementById('todo'))
+const Task = ({title, description, checked}) =>
+    (<li>{title}: {description} | {checked ? 'DONE' : 'TODO'}</li>)
+
+const App = () => (
+    <ul>
+        <h1> A fantastic Todo list is on its way !</h1>
+        {Task(testTask)}
+    </ul>
+);
+
+ReactDom.render(<App/>, document.getElementById('todo'));
