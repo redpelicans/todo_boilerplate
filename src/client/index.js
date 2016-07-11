19ec90b0f24cd46d3d80fe9c5ac913ac03c89e6b
Antoine BARBAROUX <antoine.barbaroux@redpelicans.com>
2016-07-11 10:55:39 +0200

    Added basic read-only functionnalities
    - Put fake data model in separate data.js
    - Now dynamically rendering data (_.map)
    - Also rendering each list from data
    - and each task in list
    
    TODO : patch warning about key (later)

--- src/client/index.js
+++ src/client/index.js
@@ -2,21 +2,29 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import _ from 'lodash'
 
-const testTask = {
-    id: 0,
-    title: 'test',
-    description: 'Lorem ipsum',
-    checked: false,
+// A man must have data
+import fake from './data'
+
+// A man must know how to render
+const Task = ({id, description}) => (<li>{id}: {description}</li>)
+
+const TaskList = (list) => (
+    <div>
+        <div>{list.title}</div>
+        <ul>{_.map(list.tasks, Task)}</ul>
+    </div>
+)
+
+// A man must do what he is meant to do
+const App = ({data}) => {
+    console.log(_.map(data));
+    return (
+        <div>
+            <h1> A fantastic Todo list is on its way !</h1>
+            {_.map(data, TaskList)}
+        </div>
+    );
 }
 
-const Task = ({title, description, checked}) =>
-    (<li>{title}: {description} | {checked ? 'DONE' : 'TODO'}</li>)
-
-const App = () => (
-    <ul>
-        <h1> A fantastic Todo list is on its way !</h1>
-        {Task(testTask)}
-    </ul>
-);
-
-ReactDom.render(<App/>, document.getElementById('todo'));
+// .. with a little help from his frameworks
+ReactDom.render(<App data={fake}/>, document.getElementById('todo'));
