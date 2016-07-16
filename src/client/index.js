5d1789328433d7ba987ca4b5b6e1872b0a53c435
Antoine BARBAROUX <antoine.barbaroux@redpelicans.com>
2016-07-16 12:00:22 +0200

    Slight refactor

--- src/client/index.js
+++ src/client/index.js
@@ -6,30 +6,24 @@ import _ from 'lodash'
 import fake from './data'
 
 // A man must know how to render
-const Task = ({task}) => (<li className="task flex-item">{task.id}: {task.description}</li>)
+const Task = ({task}) => (
+    <li className="task flex-item padded">{task.id}: {task.description}</li>)
 
+/**/
 const TaskList = ({list}) => (
     <div className="todo-list">
         <h3>{list.title}</h3>
-        <div className="flex-container">{_.map(list.tasks, ((task)=>(
-            <Task task={task} key={task.id} />
-        )))}</div>
-    </div>
-)
+        <div className="flex-container">{_.map(list.tasks, (task =>
+            <Task task={task} key={task.id} />))}
+        </div>
+    </div>)
 
 // A man must do what he is meant to do
-const App = ({data}) => {
-    return (
+const App = ({data}) => (
         <div className="app-wrapper">
-            <h1>A fantastic Todo list is on its way !</h1>
-            {_.map(data,
-                (taskList)=>(
-                    <TaskList list={taskList} key={taskList.id} />
-                )
-            )}
-        </div>
-    );
-}
+            <h1>A fantastic Todo list is on its way !</h1>{_.map(data, (taskList)=>(
+                <TaskList list={taskList} key={taskList.id} />))}
+        </div>)
 
 // .. with a little help from his frameworks
-ReactDom.render(<App data={fake}/>, document.getElementById('todo'));
+ReactDom.render(<App data={fake}/>, document.getElementById('react-wrapper'));
