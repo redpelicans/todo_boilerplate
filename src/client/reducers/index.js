7054abf7d7a5b1ca9b2036f3885aa9d8e381b1ca
edouard lacombe <edouard.lacombe@redpelicans.com>
2016-07-22 10:09:52 +0200

    eslint

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,21 +1,20 @@
 import { ADD_LIST, REMOVE_LIST, CHANGE_LIST } from '../actions/list';
 import { ADD_TASK, CHANGE_TASK, REMOVE_TASK } from '../actions/task';
-import _ from 'lodash'
+import _ from 'lodash';
 
 export const reducer = (state, action) => {
-  console.log(state);
   switch (action.type) {
   case ADD_LIST:
     return {
       ...state,
       lists: {
         ...state.lists,
-      [action.id]: {
-        id: action.id,
-        title: state.inputlist,
-        inputtask: '',
+        [action.id]: {
+          id: action.id,
+          title: state.inputlist,
+          inputtask: '',
+        },
       },
-    },
       inputlist: '',
     };
   case ADD_TASK:
@@ -23,40 +22,40 @@ export const reducer = (state, action) => {
       ...state,
       tasks: {
         ...state.tasks,
-      [action.idTask]: {
-        id: action.idTask,
-        idList: action.idList,
-        task: state.lists[action.idList].inputtask,
+        [action.idTask]: {
+          id: action.idTask,
+          idList: action.idList,
+          task: state.lists[action.idList].inputtask,
+        },
       },
-    },
       lists: {
         ...state.lists,
-      [action.idList]: {
-        ...state.lists[action.idList],
-        inputtask: '',
+        [action.idList]: {
+          ...state.lists[action.idList],
+          inputtask: '',
+        },
       },
-    },
     };
   case REMOVE_LIST:
     return {
       ...state,
-      lists: (_.omit(state.lists, action.idList))
+      lists: (_.omit(state.lists, action.idList)),
     };
   case REMOVE_TASK:
     return {
       ...state,
-      tasks: (_.omit(state.tasks, action.idTask))
+      tasks: (_.omit(state.tasks, action.idTask)),
     };
   case CHANGE_TASK:
     return {
       ...state,
       lists: {
         ...state.lists,
-      [action.idList]: {
-        ...state.lists[action.idList],
-        inputtask: action.inputtask,
+        [action.idList]: {
+          ...state.lists[action.idList],
+          inputtask: action.inputtask,
+        },
       },
-    },
     };
   case CHANGE_LIST:
     return {
