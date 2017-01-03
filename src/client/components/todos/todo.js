068fab993738caf0fff6e56407ade9db916946f3
Matthias Leconte <matt@MacBook-Pro-de-Matthias.local>
2017-01-03 16:17:10 +0100

    Init model.js which contains methods about data normalization. + getTasksByTodoId method.

--- src/client/components/todos/todo.js
+++ src/client/components/todos/todo.js
@@ -1,24 +1,27 @@
 import React, { PropTypes } from 'react';
 import Task from './task';
+import getTasksByTodoId from '../../model';
 
-const Todo = ({ title, tasks, actions }) =>
+const Todo = ({ todo, tasks, actions }) =>
   <div className="todo">
     <header>
       <h1>
-        {title}
+        {todo.title}
       </h1>
       <button onClick={actions.delTodo}>
         del
       </button>
     </header>
     <div className="todo-body">
-      {tasks.map(task => <Task title={task.title} isChecked={task.isChecked} actions={actions} key={task.id} />)}
+      {getTasksByTodoId(tasks, todo.id).map(task =>
+        <Task title={task.title} isChecked={task.isChecked} actions={actions} key={task.id} />
+      )}
     </div>
   </div>
 ;
 
 Todo.propTypes = {
-  title: PropTypes.string.isRequired,
+  todo: PropTypes.object,
   tasks: PropTypes.array,
   actions: PropTypes.object,
 };
