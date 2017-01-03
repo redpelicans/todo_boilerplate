4db503be16dfeaa238ed4bb9713f9bffa5652427
eric <eric.basley@redpelicans.com>
2017-01-03 13:57:50 +0100

    use inhouse store

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -12,10 +12,39 @@ const Wrapper = styled.section`
   background: papayawhip;
 `;
 
-const App = () => (
-  <Wrapper>
-    <Title>Hello World, this is my first react app!</Title>
-  </Wrapper>
+const Todos = ({todos, dispatch, onDelete}) =>  (
+  <div>
+    {todos.map(todo => (
+      <div key={todo.id}>
+        <button onClick={() => dispatch(onDelete(todo.id))}>
+          {todo.title}
+        </button>
+      </div>
+    ))}
+  </div>
 );
+
+class App extends React.Component {
+  componentDidMount() {
+    const { store } = this.props;
+    store.listen(() => this.forceUpdate());
+  }
+
+  onAdd = () => {
+    const { actions, store } = this.props;
+    store.dispatch(actions.addTodo());
+  }
+
+  render() {
+    const { store, actions } = this.props;
+    return (
+      <Wrapper>
+        <Title>Hello World, this is my first react app!</Title>
+        <button onClick={this.onAdd}> Add </button>
+        <Todos todos={store.state.todos} dispatch={store.dispatch.bind(store)} onDelete={actions.deleteTodo} />
+      </Wrapper>
+    )
+  }
+}
 
 export default App;
