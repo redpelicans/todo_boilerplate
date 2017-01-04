50082e2e137369e6e45c1f4389e2e0ffdf4de80b
eric <eric.basley@redpelicans.com>
2017-01-04 17:05:17 +0100

    Update tests, and migrate them to __tests__ folders

--- src/client/components/App/index.js
+++ src/client/components/App/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Title = styled.h1`
+export const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
   color: palevioletred;
--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/app';
+import App from './components/App';
 
-console.log('mounting react app ...');  // eslint-disable-line no-console 
+console.log('mounting react app ...');  // eslint-disable-line no-console
 render(<App />, document.getElementById('__TODO__'));
 
