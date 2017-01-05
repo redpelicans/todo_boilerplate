bc7e51e35afac998c00d1182d7a5d1430ddaf22a
Matthias Leconte <matt@MacBook-Pro-de-Matthias.local>
2017-01-05 10:41:26 +0100

    Actions, Store and MockUp have their own file.

---
+++ src/client/actions.js
@@ -0,0 +1,54 @@
+const actions = {
+  addTodo(name) {
+    console.log('add todo ', name); // eslint-disable-line no-console
+    return state => ({
+      ...state,
+      todos: state.todos.concat({
+        title: name,
+        id: state.todos[state.todos.length - 1].id + 1,
+      }),
+    });
+  },
+  delTodo(id) {
+    console.log('del todo ', id); // eslint-disable-line no-console
+    return state => ({
+      ...state,
+      todos: state.todos.filter(todo => todo.id !== id),
+      tasks: state.tasks.filter(task => task.todoId !== id),
+    });
+  },
+  addTask(task) {
+    console.log('add task ', task); // eslint-disable-line no-console
+    return state => ({
+      ...state,
+      tasks: state.tasks.concat({
+        ...task,
+        id: state.tasks[state.tasks.length - 1].id + 1,
+        isChecked: false,
+      }),
+    });
+  },
+  editTask(editedTask) {
+    console.log('edit task ', editedTask.id); // eslint-disable-line no-console
+    return state => ({
+      ...state,
+      tasks: state.tasks.map(task => ((task.id === editedTask.id) ? editedTask : task)),
+    });
+  },
+  delTask(id) {
+    console.log('del task ', id); // eslint-disable-line no-console
+    return state => ({
+      ...state,
+      tasks: state.tasks.filter(task => task.id !== id),
+    });
+  },
+  switchMode() {
+    console.log('switch mode'); // eslint-disable-line no-console
+    return state => ({
+      ...state,
+      showCompletedTodos: !state.showCompletedTodos,
+    });
+  },
+};
+
+export default actions;
